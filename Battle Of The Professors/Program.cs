8de3ad94260dbf813113ca7f1ceb01f8eb762398
Mason <Mason@DESKTOP-NHDIDLB>
2021-01-30 18:19:48 -0600

    Add project files.

---
+++ Battle Of The Professors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Of_The_Professors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a game");
+            Console.ReadKey();
+        }
+    }
+}
