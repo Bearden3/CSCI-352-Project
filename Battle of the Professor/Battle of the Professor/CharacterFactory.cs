dee01eab0207078db321e751798e54de29a4af4d
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-03-26 13:55:45 -0500

    Added CharacterFactory and ICharacterFactory.

---
+++ Battle of the Professor/Battle of the Professor/CharacterFactory.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Author: Jordan Taylor
+ * Date: 03 / 26 / 2021
+ * Description: Creates a player and professor using ICharacterFactory.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_of_the_Professor
+{
+    class PlayerFactory : ICharacterFactory
+    {
+        public PlayerFactory()
+        {
+            int health = 0;
+            int sanity = 0;
+            int intellect = 0;
+        }
+
+        public void CreateCharacter()
+        {
+            // TODO: Create a player.
+        }
+    }
+
+    class ProfessorFactory : ICharacterFactory
+    {
+        public ProfessorFactory()
+        {
+            int health = 0;
+            int sanity = 0;
+            int intellect = 0;
+        }
+
+        public void CreateCharacter()
+        {
+            // TODO: Create a professor.
+        }
+    }
+}
---
+++ Battle of the Professor/Battle of the Professor/ICharacterFactory.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Author: Jordan Taylor
+ * Date: 03 / 26 / 2021
+ * Description: Allows CharacterFactory to create characters.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_of_the_Professor
+{
+    interface ICharacterFactory
+    {
+        void CreateCharacter();
+    }
+}
