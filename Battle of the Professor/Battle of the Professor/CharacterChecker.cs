03d89263d56ff57c3cb9a3a555a4648edb4d3d90
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-04-06 00:55:14 -0500

    Update

---
+++ Battle of the Professor/Battle of the Professor/CharacterChecker.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Author: Jordan Taylor
+ * Date: 03 / 26 / 2021
+ * Description: Creates a player and professor using ICharacterFactory.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_of_the_Professor
+{
+    enum Characters { PROFESSOR, STUDENT };
+
+    class CharacterCheck
+    {
+        Characters character;
+        ICharacterFactory factory;
+
+        // stat values for Player. The underscores '_' are in place to help distinguish a difference from the arguments.
+        int _health = 0;
+        int _sanity = 0;
+        int _intellect = 0;
+
+        public CharacterCheck(Characters c)
+        {
+            character = c;
+        }
+
+        public void CharacterAssign(int health, int sanity, int intellect) // These arguments will help update the values through main.
+        {
+            _health = health;
+            _sanity = sanity;
+            _intellect = intellect;
+
+            switch (character)
+            {
+                case Characters.PROFESSOR:
+                    factory = new ();
+                    break;
+                case Characters.STUDENT:
+                    factory = new HTCFactory();
+                    break;
+            }
+        }
+    }
+}
